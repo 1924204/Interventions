b975e6fbb954e67020bae3baf9a7de167c162032
1924204 <70345234+1924204@users.noreply.github.com>
2021-03-18 19:26:22 -0400

    Fait : Zone PRÉNOM invalide avec aucune valeur

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -25,9 +25,34 @@ describe('ProblemeComponent', () => {
   //   expect(component).toBeTruthy();
   // });
 
+  it('Zone PRÉNOM invalide avec 2 caractères', () => {
+    let errors = {};
+    let zone = component.problemeForm.get('prenom');
+    zone.setValue('a'.repeat(2));
+    errors = zone.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+  });
+
   it('Zone PRÉNOM valide avec 3 caractères', () => {
-    let zone = component.problemeForm.controls['prenom'];
+    let errors = {};
+    let zone = component.problemeForm.get('prenom');
+    zone.setValue('a'.repeat(3));
+    errors = zone.errors || {};
+    expect(errors['minlength']).toBeFalsy();
+  });
+
+  it('Zone PRÉNOM valide avec 200 caractères', () => {
+    let errors = {};
+    let zone = component.problemeForm.get('prenom');
     zone.setValue('a'.repeat(200));
-    expect(zone.valid).toBeTruthy();
+    errors = zone.errors || {};
+    expect(errors['minlength']).toBeFalsy();
+  });
+
+  it('Zone PRÉNOM invalide avec aucune valeur', () => {
+    let errors = {};
+    let zone = component.problemeForm.get('prenom');
+    errors = zone.errors || {};
+    expect(errors['required']).toBeTruthy();
   });
 });
