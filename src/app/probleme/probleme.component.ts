c31ef013f7c682538b52f4414a685f20c0cc9aeb
1924204 <70345234+1924204@users.noreply.github.com>
2021-03-25 21:29:18 -0400

    Fait: Travail 10 partie 1

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LongueurValidator } from '../shared/longueur-minimum/longueur-minimum.component';
+import { ITypeProbleme } from './typeprobleme';
+import { TypeproblemeService } from './typeprobleme.service';
 
 @Component({
   selector: 'Inter-probleme',
   templateUrl: './probleme.component.html',
-  styleUrls: ['./probleme.component.css']
+  styleUrls: ['./probleme.component.css'],
 })
 export class ProblemeComponent implements OnInit {
   problemeForm: FormGroup;
+  typesProblemes: ITypeProbleme[];
+  errorMessage: String;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private problemes: TypeproblemeService) { }
 
   ngOnInit(): void {
     this.problemeForm = this.fb.group({
       prenom: ['',[LongueurValidator.longueurMinimum(3), Validators.required]]
-    })
+    });
+
+    this.problemes.obtenirTypes()
+    .subscribe(type => this.typesProblemes = type,
+               error => this.errorMessage = <any>error);  
   }
 
   save(): void{
---
+++ src/app/probleme/typeprobleme.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { ITypeProbleme } from './typeprobleme';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//private baseUrl = 'api/typesprobleme';
+
+export class TypeproblemeService {
+
+  constructor(private http: HttpClient) { }
+
+  obtenirTypes(): Observable<ITypeProbleme[]> {
+    return this.http.get<ITypeProbleme[]>('api/typesprobleme').pipe(
+        tap(data => console.log('obtenirTypes: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+        );
+}
+
+
+private handleError(err: HttpErrorResponse) {
+  // in a real world app, we may send the server to some remote logging infrastructure
+  // instead of just logging it to the console
+  let errorMessage = '';
+  if (err.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    errorMessage = `An error occurred: ${err.error.message}`;
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+  }
+  console.error(errorMessage);
+  return throwError(errorMessage);
+}
+}
