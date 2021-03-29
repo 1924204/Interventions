ee7ca831f6c6f3c12efea16051de78bc19986237
1924204 <70345234+1924204@users.noreply.github.com>
2021-03-29 16:46:52 -0400

    Travail 10

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -18,7 +18,9 @@ export class ProblemeComponent implements OnInit {
 
   ngOnInit(): void {
     this.problemeForm = this.fb.group({
-      prenom: ['',[LongueurValidator.longueurMinimum(3), Validators.required]]
+      prenom: ['',[LongueurValidator.longueurMinimum(3), Validators.required]],
+      nom: ['',[LongueurValidator.longueurMinimum(1),LongueurValidator.longeurMaximum(50), Validators.required]],
+      noType: ['', Validators.required]
     });
 
     this.problemes.obtenirTypes()
--- src/app/shared/longueur-minimum/longueur-minimum.component.ts
+++ src/app/shared/longueur-minimum/longueur-minimum.component.ts
@@ -1,6 +1,17 @@
 import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export class LongueurValidator {
+    static longeurMaximum(longeurMaximum: number): any {
+        return (valeurControle: AbstractControl): { [key: string]: boolean } | null => { 
+            if(valeurControle.value != null) {
+                if (valeurControle.value.trim().length <= longeurMaximum) {
+                    return null;
+                }
+            }
+            
+            return { 'nbreCaracteresTrop': true};
+    }
+    }
     static longueurMinimum(longueurMinimum: number) : ValidatorFn {
         //Sous ANGULAR dans les validateurs pour indiquer un succes retourne null
         return (valeurControle: AbstractControl): { [key: string]: boolean } | null => { 
