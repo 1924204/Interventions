358307879b72b0372bd99a611afbbdaa37d9ee2c
1924204 <70345234+1924204@users.noreply.github.com>
2021-03-29 21:42:11 -0400

    Travail #11

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -20,7 +20,12 @@ export class ProblemeComponent implements OnInit {
     this.problemeForm = this.fb.group({
       prenom: ['',[LongueurValidator.longueurMinimum(3), Validators.required]],
       nom: ['',[LongueurValidator.longeurMaximum(50), Validators.required]],
-      noType: ['', Validators.required]
+      noType: ['', Validators.required],
+      courrielGroup: this.fb.group({
+        courriel: [{value:'', disabled: true}],
+        courrielConfirmation: [{value:'', disabled: true}],
+      }),
+      telephone: [{value:'', disabled: true}]
     });
 
     this.problemes.obtenirTypes()
@@ -28,6 +33,41 @@ export class ProblemeComponent implements OnInit {
                error => this.errorMessage = <any>error);  
   }
 
+
+  appliquerNotifications(typeCueillette: string): void {
+    const courrielControl = this.problemeForm.get('courrielGroup.courriel');
+    const courrielConfirmationControl = this.problemeForm.get('courrielGroup.courrielConfirmation');   
+    const courrielGroupControl = this.problemeForm.get('courrielGroup');      
+
+    // Tous remettre à zéro
+    courrielControl.clearValidators();
+    courrielControl.reset();  // Pour enlever les messages d'erreur si le controle contenait des données invaldides
+    courrielControl.disable();  
+
+    courrielConfirmationControl.clearValidators();
+    courrielConfirmationControl.reset();    
+    courrielConfirmationControl.disable();
+
+    if (typeCueillette === 'Courriel') {   
+      courrielControl.setValidators([Validators.required]);      
+      courrielControl.enable();  
+      courrielConfirmationControl.setValidators([Validators.required]);              
+      courrielConfirmationControl.enable();  
+            // Si le validateur est dans un autre fichier l'écire sous la forme suivante : 
+            // ...Validators.compose([classeDuValidateur.NomDeLaMethode()])])
+      
+      }   
+      else
+      {
+        if(typeCueillette === 'Inconnu')
+        {
+          courrielControl.setValidators([Validators.required]);      
+          courrielControl.disable();           
+        }
+      }
+      courrielControl.updateValueAndValidity();   
+      courrielConfirmationControl.updateValueAndValidity();         
+  }
   save(): void{
       
   }
